1e7177901f0d05766949beb105838a0a0ae822ee
Pepe Valdivia <jvaldivia@softweb.pe>
2017-07-26 15:02:02 -0500

    CORS habilitado para el uso de archivos estáticos en /public

--- src/main/java/pe/softweb/app/App.java
+++ src/main/java/pe/softweb/app/App.java
@@ -11,6 +11,9 @@ public class App{
     public static void main(String[] args) {
 	    	exception(Exception.class, (e, req, res) -> e.printStackTrace()); 
 	    	staticFiles.location("/public");
+	    	staticFiles.header("Access-Control-Allow-Origin", "*");
+	    	staticFiles.header("Access-Control-Request-Method",  "*");
+	    	staticFiles.header("Access-Control-Allow-Headers",  "*");
 	    	port(2000);
 	    	
 	    	options("/*", (request, response) -> {
@@ -31,6 +34,7 @@ public class App{
 	        response.header("Access-Control-Allow-Origin", "*");
 	        response.header("Access-Control-Request-Method",  "*");
 	        response.header("Access-Control-Allow-Headers",  "*");
+	        response.header("Access-Control-Allow-Credentials", "true");
 	        response.header("Server",  "Ubuntu, Jetty");
 	        // Note: this may or may not be necessary in your particular application
 	        //response.type("application/json");
--- src/main/java/pe/softweb/utils/Constantes.java
+++ src/main/java/pe/softweb/utils/Constantes.java
@@ -11,16 +11,4 @@ public class Constantes {
 		
 		return mapita;
 	}
-	
-	public static String get(String llave){
-		HashMap<String, String> mapita = new HashMap<String, String>();
-		mapita.put("BASE_URL", "http://127.0.0.1:2000/");
-		mapita.put("STATIC_URL", "http://127.0.0.1:2000/");
-		
-		if(mapita.containsKey(llave)){
-			return mapita.get(llave);
-		}else{
-			return null;
-		}
-	}
 }
